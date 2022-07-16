2ca4cd5957a2243d450f3e18259ad93cc19c635b
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-07-16 09:18:43 +0430

    food_delivery_ui

---
+++ lib/styles/styles_color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+final backGroundColor=Colors.grey[100];
+const color1=Color(0XFFD74D47);
+const color2=Color(0XFFa13630);
+
+
+const textStyle1=TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 26,);
+const textStyle2=TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 20,);
+const textStyle3=TextStyle(color: Colors.black,fontWeight: FontWeight.normal,fontSize: 20,);
+const textStyle4=TextStyle(color: Colors.white,fontWeight: FontWeight.w600,fontSize: 22,);
+const textStyle5=TextStyle(color: Colors.black,fontWeight: FontWeight.w500,fontSize: 16,);
+const textStyle6=TextStyle(color: Colors.grey,fontWeight: FontWeight.normal,fontSize: 18,);
+const textStyle7=TextStyle(color: Colors.grey,fontWeight: FontWeight.bold,fontSize: 16,);
+const textStyle8=TextStyle(color: Colors.grey,fontWeight: FontWeight.normal,fontSize: 14,);
+const textStyle9=TextStyle(color: color2,fontWeight: FontWeight.bold,fontSize: 26,);
